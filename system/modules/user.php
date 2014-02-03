43de4a02890d0b2fb43a82e6e1ea7d9fd66afc1a
dkonasov <d.konasov@yandex.ru>
2014-02-03 01:22:20 -0800

    ver. 1.4
    added new user module functions: setusercookie($mail, $password, $longsession(bool))
    fixed problem with blank email field in adduser function.

--- system/modules/user.php
+++ system/modules/user.php
@@ -6,9 +6,9 @@ require_once './system/functions/dbconn.php';
 
 function adduser($mail, $password) //Добавление нового пользователя
 {
+dbconn('utf-8');
 $mail=sanitise($mail);
 $password=encrypt($password);
-dbconn('utf-8');
 $query='INSERT INTO users(email, pass) VALUES("'.$mail.'", "'.$password.'");';
 $r=mysql_query($query) or die(mysql_error());
 return $r;
@@ -30,6 +30,35 @@ else
 $result=false;
 }
 return $result;
+}
+
+function setusercookie($email, $password, $longsession) {
+$password=encrypt($password);
+if($longsession) {
+setcookie("email",$email, time()+3600000);
+setcookie("password",$password, time()+3600000);
+}
+
+else {
+setcookie("email",$email);
+setcookie("password",$password);
+}
+
+}
+
+function checkusercookie() {
+
+if($_COOKIE['email']&& $_COOKIE['password'])
+{
+dbconn('utf-8');
+$email=sanitise($_COOKIE['email']);
+$q='SELECT email, pass FROM users WHERE email="'.$email.'" AND pass="'.$_COOKIE['password'].'"';
+$r=mysql_query($q) or die(mysql_error);
+if(mysql_num_rows($r)>0) return true;
+else return false;
+}
+else return false;
+
 }
 
 ?>
